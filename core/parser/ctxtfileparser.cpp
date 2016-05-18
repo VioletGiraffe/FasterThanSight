f746783f340d3540484c0d660ade353671e5f550
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-05-18 10:45:28 +0300

    Crude greedy text parsing code added

--- core/parser/ctxtfileparser.cpp
+++ core/parser/ctxtfileparser.cpp
@@ -16,7 +16,7 @@ std::vector<TextFragment> CTxtFileParser::parse(QIODevice& device)
 	const QString text = readText(device);
 
 	struct Delimiter {
-		QString delimiterCharacter;
+		QChar delimiterCharacter;
 		TextFragment::Delimiter delimiterType;
 
 		inline bool operator< (const Delimiter& other) const {
@@ -30,13 +30,15 @@ std::vector<TextFragment> CTxtFileParser::parse(QIODevice& device)
 		{':', TextFragment::Colon},
 		{';', TextFragment::Semicolon},
 		{',', TextFragment::Comma},
+		// TODO: dash should be ignored unless it has an adjacent space!
 		{'-', TextFragment::Dash},
-		{"...", TextFragment::Ellipsis},
-		{"⋯", TextFragment::Ellipsis},
-		{"…", TextFragment::Ellipsis},
-		{"!", TextFragment::ExclamationMark},
-		{"\n", TextFragment::Newline},
-		{"?", TextFragment::QuestionMark},
+		// TODO:
+		// {"...", TextFragment::Ellipsis},
+		{'⋯', TextFragment::Ellipsis},
+		{'…', TextFragment::Ellipsis},
+		{'!', TextFragment::ExclamationMark},
+		{'\n', TextFragment::Newline},
+		{'?', TextFragment::QuestionMark},
 
 		{')', TextFragment::Bracket},
 		{'(', TextFragment::Bracket},
@@ -47,6 +49,34 @@ std::vector<TextFragment> CTxtFileParser::parse(QIODevice& device)
 	};
 
 	std::vector<TextFragment> fragments;
+
+	QString buffer;
+	bool wordEnded = false;
+	TextFragment::Delimiter lastDelimiter = TextFragment::Space;
+	for (QChar ch: text)
+	{
+		if (ch == '\r')
+			continue;
+
+		const auto it = delimiters.find({ch, TextFragment::Space});
+		if (it == delimiters.end()) // Not a delimiter
+		{
+			if (wordEnded) // This is the first letter of a new word
+			{
+				fragments.emplace_back(buffer, lastDelimiter);
+				wordEnded = false;
+				buffer = ch;
+			}
+			else
+				buffer += ch;
+		}
+		else // This is a delimiter. Append it to the current word.
+		{
+			lastDelimiter = it->delimiterType;
+			wordEnded = true;
+			buffer += ch;
+		}
+	}
 
 	return fragments;
 }
@@ -62,7 +92,7 @@ QString CTxtFileParser::readText(QIODevice& device)
 	if (!device.isOpen())
 		assert_and_return_r(device.open(QIODevice::ReadOnly), QString());
 
-	QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+	QTextCodec* codec = QTextCodec::codecForName("UTF-8");
 	if (!codec)
 		return QString();
 
@@ -73,5 +103,8 @@ QString CTxtFileParser::readText(QIODevice& device)
 		return text; // Valid UTF-8 text
 
 	codec = QTextCodec::codecForLocale();
+	if (!codec)
+		return QString();
+
 	return codec->toUnicode(textData);
 }
