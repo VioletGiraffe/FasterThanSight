16c2f2f5caf2f4097b76cd3eb4383cbafc937ce1
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-07-07 00:28:14 +0300

    CTextBrowser: highlighting the current chapter

--- app/src/widgets/ctextbrowser.cpp
+++ app/src/widgets/ctextbrowser.cpp
@@ -84,15 +84,33 @@ bool CTextBrowser::eventFilter(QObject * o, QEvent * e)
 	if (o == ui->_textView && e->type() == QEvent::MetaCall) // QEvent::MetaCall, whatever it is, seems to be the only event that occurs when scrolling the QPlainTextEdit
 	{
 		const int character = ui->_textView->cursorForPosition({ui->_textView->width() / 2, ui->_textView->height() / 2}).position();
+		int currentChapterItemIndex = -1;
+
 		for (int i = ui->_chaptersList->count() - 1; i >= 0; --i)
 		{
 			const int chapterStartCharacterIndex = ui->_chaptersList->item(i)->data(Qt::UserRole).toInt();
 			if (chapterStartCharacterIndex < character)
 			{
 				qDebug() << ui->_chaptersList->item(i)->text();
-				return QDialog::eventFilter(o, e);
+				currentChapterItemIndex = i;
+				break;
 			}
 		}
+
+		for (int i = 0, count = ui->_chaptersList->count(); i < count; ++i)
+		{
+			auto item = ui->_chaptersList->item(i);
+			QFont f = item->font();
+			f.setBold(i == currentChapterItemIndex);
+			item->setFont(f);
+			if (i == currentChapterItemIndex)
+				item->setBackground(QColor(255, 220, 220));
+			else
+				item->setBackground(Qt::NoBrush);
+		}
+
+		if (currentChapterItemIndex >= 0)
+			ui->_chaptersList->scrollToItem(ui->_chaptersList->item(currentChapterItemIndex));
 	}
 
 	return QDialog::eventFilter(o, e);
