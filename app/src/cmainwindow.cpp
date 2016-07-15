e46fea3a48eebb37a9aedc7faaa06a9608e6bd0b
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-07-16 00:14:36 +0300

    Focus issues fixed - clicking CReaderView sets focus to it

--- app/src/cmainwindow.cpp
+++ app/src/cmainwindow.cpp
@@ -137,6 +137,7 @@ bool CMainWindow::eventFilter(QObject* /*o*/, QEvent* e)
 	switch (e->type())
 	{
 	case QEvent::MouseButtonRelease:
+		ui->_text->setFocus();
 		_reader.togglePause();
 		break;
 	case QEvent::MouseButtonDblClick:
