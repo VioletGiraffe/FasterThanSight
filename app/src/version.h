33078cc24c057d819bdc6878a61094e9f763b7f4
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-07-19 14:00:00 +0300

    1.2.2

--- app/src/version.h
+++ app/src/version.h
@@ -6,5 +6,5 @@ DISABLE_COMPILER_WARNINGS
 #include <QString>
 RESTORE_COMPILER_WARNINGS
 
-#define VERSION_STRING QStringLiteral("1.2.1")
+#define VERSION_STRING QStringLiteral("1.2.2")
 #define REPO_ADDRESS QStringLiteral("https://github.com/VioletGiraffe/FasterThanSight")
