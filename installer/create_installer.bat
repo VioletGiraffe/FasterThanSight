867a27cba73a06161d4092982aa965006c4052dd
VioletGiraffe <noreply@bitbucket.org>
2016-10-05 23:09:44 +0300

    Building Windows installer with MSVC2015

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -2,7 +2,7 @@ REM this script must set QTDIR32 path to the root of the Qt folder. Example:
 REM set QTDIR32=k:\Qt\5\5.4\msvc2013_opengl\
 
 call set_qt_paths.bat
-set VS_TOOLS_DIR=%VS120COMNTOOLS%
+set VS_TOOLS_DIR=%VS140COMNTOOLS%
 
 SETLOCAL
 
@@ -13,7 +13,7 @@ pushd ..\
 popd
 
 call "%VS_TOOLS_DIR%VsDevCmd.bat" x86
-msbuild ..\FasterThanSight.sln /t:Build /p:Configuration=Release;PlatformToolset=v120
+msbuild ..\FasterThanSight.sln /t:Build /p:Configuration=Release;PlatformToolset=v140
 
 xcopy /R /Y ..\bin\release\FasterThanSight.exe binaries\
 xcopy /R /Y "3rdparty binaries"\Win\32\* binaries\
@@ -23,8 +23,9 @@ SET PATH=%QTDIR32%\bin\
 %QTDIR32%\bin\windeployqt.exe --dir binaries\Qt --force --no-translations --release --no-compiler-runtime --no-angle binaries\FasterThanSight.exe
 ENDLOCAL
 
-xcopy /R /Y %SystemRoot%\SysWOW64\msvcr120.dll binaries\msvcr\
-xcopy /R /Y %SystemRoot%\SysWOW64\msvcp120.dll binaries\msvcr\
+xcopy /R /Y %SystemRoot%\SysWOW64\msvcp140.dll binaries\msvcr\
+xcopy /R /Y %SystemRoot%\SysWOW64\vcruntime140.dll binaries\msvcr\
+xcopy /R /Y "%programfiles(x86)%\Windows Kits\10\Redist\ucrt\DLLs\x86\*" binaries\msvcr\
 
 del binaries\Qt\opengl*.*
 
